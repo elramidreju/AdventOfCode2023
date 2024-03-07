85adeccedb372cb2c33a40c1dbafaadcf3c8c3a9
Pablo <pgarciasalmones@gmail.com>
2024-03-07 17:57:01 +0100

    Objectives in comments First draft of first test

--- Day03.Tests/Day03.Tests.cpp
+++ Day03.Tests/Day03.Tests.cpp
@@ -1,16 +1,33 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+#include <tuple>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+using Position = std::tuple<int, int>;
 
 namespace Day03Tests
 {
 	TEST_CLASS(Day03Tests)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		// Objetivo: Suma de todos los PartNumber
+		// - ¿Qué es un PartNumber?
+		// -- Es un número adyacente a un símbolo
+		// --- Qué es un símbolo?
+		// ----	Es cualquier carácter distinto de . o un número
+		// - ¿Qué es un número?
+		// -- Varios dígitos unidos horizontalmente (sin otros símbolos entre ellos)
+		// - ¿Qué es ser adyacente?
+		// -- Es estar en una casilla contigua vertical, horizontal o diagonalmente (1 unidad de distancia)
+
+		TEST_METHOD(AdjacentPosition)
 		{
+			Position expected = Position(0, 1);
+			Assert::IsTrue(qweda::AdjacentTo(Position(0, 0)).Size() == 8);
 		}
+
 	};
 }
