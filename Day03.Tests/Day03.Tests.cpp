5ba60ab173a524d34165743022b12b6c384a3d82
Pablo <pgarciasalmones@gmail.com>
2024-03-07 17:26:57 +0100

    New test project for Day03

---
+++ Day03.Tests/Day03.Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Day03Tests
+{
+	TEST_CLASS(Day03Tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
