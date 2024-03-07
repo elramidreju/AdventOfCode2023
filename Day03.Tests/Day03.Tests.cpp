76b14132b25f310ec924e71b64ddbda8e75d8649
Iván Moreno <imoreno.owo@gmail.com>
2024-03-07 18:02:44 +0100

    feat: return vector with size of adjacent parts

--- Day03.Tests/Day03.Tests.cpp
+++ Day03.Tests/Day03.Tests.cpp
@@ -2,10 +2,20 @@
 #include "CppUnitTest.h"
 
 #include <tuple>
+#include <vector>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 using Position = std::tuple<int, int>;
+
+static class qweda
+{
+public:
+	static std::vector<Position> AdjacentTo(Position aPosition)
+	{
+		return std::vector<Position>(8);
+	}
+};
 
 namespace Day03Tests
 {
@@ -26,8 +36,9 @@ namespace Day03Tests
 		TEST_METHOD(AdjacentPosition)
 		{
 			Position expected = Position(0, 1);
-			Assert::IsTrue(qweda::AdjacentTo(Position(0, 0)).Size() == 8);
+			Assert::IsTrue(qweda::AdjacentTo(Position(0, 0)).size() == 8);
 		}
 
 	};
+
 }
